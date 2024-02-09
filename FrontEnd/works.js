511228e7609cebd6ff84497a444dd0dd1f6b0aeb
Johann <Joh.rackelboom@gmail.com>
2024-02-09 12:55:20 +0100

    add filter

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -5,7 +5,6 @@ function genererWorks(works){
     for (let i = 0; i < works.length; i++) {
         
         const work = works[i];
-        console.log(work)
 
         const sectionGallery = document.querySelector(".gallery");
         const workElement = document.createElement("work");
@@ -22,3 +21,37 @@ function genererWorks(works){
 
 genererWorks(works);
 
+
+const buttonAll = document.getElementById("all");
+
+buttonAll.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(works);
+});
+
+const buttonTrinkets = document.getElementById("trinkets");
+
+buttonTrinkets.addEventListener("click", function () {
+    const workfilter = works.filter((work) => work.categoryId == 1);
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(workfilter);
+});
+
+const buttonFlats = document.getElementById("flats");
+
+buttonFlats.addEventListener("click", function () {
+    const workfilter = works.filter((work) => work.categoryId == 2);
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(workfilter);
+});
+
+const buttonBuildings = document.getElementById("buildings");
+
+buttonBuildings.addEventListener("click", function () {
+    const workfilter = works.filter((work) => work.categoryId == 3);
+
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(workfilter);
+});
