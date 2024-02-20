ea8b1496d2e59940bc2d9a4864bed5ce6f4300b4
Johann <Joh.rackelboom@gmail.com>
2024-02-20 16:55:52 +0100

    update login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,5 +1,7 @@
 const loginClient = document.querySelector(".loginSubmit");
+
 loginClient.addEventListener("submit", async function (event) {
+
     event.preventDefault();
     const loginID = {
         email: event.target.querySelector("[name=email]").value,
@@ -11,14 +13,14 @@ loginClient.addEventListener("submit", async function (event) {
         headers: { "Content-Type": "application/json" },
         body: loginCharge
     })
+
     console.log(user)
-    async function verifierRequete() {
-        try {
-        const response = await fetch("http://localhost:5678/api/users/login");
-        console.log(response.status)
-        console.log(user)
-        if (response.status === 200) {
-            window.localStorage.setItem("tokenID", body.token );
+
+    try {
+        const userBody = await user.json()
+        console.log(userBody.token)
+        if (user.status === 200) {
+            window.localStorage.setItem("tokenID", userBody.token );
             window.location.href="./index.html";
         } else {
             let errorMessage = document.querySelector(".errorMessage");
@@ -30,30 +32,7 @@ loginClient.addEventListener("submit", async function (event) {
                 forgotPassword.insertBefore(errorMessage, forgotPassword.firstChild); 
             }
         }
-        } catch (error) {
-        console.error('Une erreur est survenue lors de la requête : ', error);
-        }
+    } catch (error) {
+        console.error("Une erreur est survenue lors de la requête : ", error);
     }
-verifierRequete();
-const body = user.json();
-console.log(body)
-console.log(user)
 });
-
-/*await fetch("http://localhost:5678/api/users/login")
-.then(response => {
-        console.log(response.status)
-        if (response.status === 200) {
-        window.localStorage.setItem("token", body.token );
-        window.location.href="./index.html";
-        } else {
-            let errorMessage = document.querySelector(".errorMessage"); 
-            if (!errorMessage) {
-                errorMessage = document.createElement("p");
-                errorMessage.classList.add('errorMessage')
-                errorMessage.innerText = "Erreur dans l'identifiant ou le mot de passe";
-                let forgotPassword = document.getElementById("forgotPassword") 
-                forgotPassword.insertBefore(errorMessage, forgotPassword.firstChild); 
-            }
-    }
-});*/
