f2597d794ed012a72cd852c7fdd85c8a85bb6cc2
Johann <Joh.rackelboom@gmail.com>
2024-02-27 14:18:17 +0100

    add fetch"Post",NewWork (don't work)

--- FrontEnd/edit.js
+++ FrontEnd/edit.js
@@ -1,5 +1,7 @@
 import { displayWorks, works } from "./works.js";
 
+let tokenID = localStorage.getItem("tokenID")
+
 export function editPage() {
     let iconHeader = document.createElement("i");
     iconHeader.classList.add("far", "fa-pen-to-square");
@@ -41,6 +43,215 @@ export function editPage() {
     };
 };
 
+function previous() { 
+        
+    let previousPage = document.querySelector(".previousPage")
+    if (!previousPage) {
+            
+        previousPage = document.createElement("i")
+        previousPage.classList.add("fa-solid", "fa-arrow-left")
+        previousPage.classList.add("previousPage")
+        let iconCloseModal = document.querySelector(".closeModal")
+        iconCloseModal.parentNode.insertBefore(previousPage, iconCloseModal);
+    }
+    previousPage.addEventListener("click", function () {
+        document.querySelector("#displayModal").style.display = "inherit";
+        document.querySelector(".divForm").style.display = "none";
+        previousPage.remove()
+    })
+    
+}
+
+function checkFormValidity() {
+    const fileInput = document.getElementById('fileInput');
+    const titleInput = document.getElementById('titleInput');
+    const categorySelect = document.getElementById('categorySelect');
+    const submitButton = document.getElementById('submitButton');
+  
+    if (fileInput.files.length > 0 && titleInput.value.trim() !== '' && categorySelect.value !== '') {
+      submitButton.removeAttribute('disabled');
+    } else {
+      submitButton.setAttribute('disabled', true);
+    }
+}
+
+function newWork() {
+    previous()
+    document.querySelector("#displayModal").style.display = "none";
+    let divForm = document.querySelector(".divForm")
+    if (!divForm) {
+        divForm = document.createElement("div")
+        divForm.classList.add("divForm")
+        document.querySelector("#divModal").appendChild(divForm)
+
+        let titleForm = document.createElement("h1")
+        titleForm.innerText = "Ajout photo"
+        titleForm.classList.add("titleForm")
+        divForm.appendChild(titleForm)
+
+        let form = document.createElement ("form");
+        form.id = "formProject";
+        form.method = "POST";
+        form.enctype = "multipart/form-data";
+
+        let divAddImg = document.createElement("div")
+        divAddImg.classList.add("divAddImg")
+        form.appendChild(divAddImg)
+
+        let divInput = document.createElement("div")
+        divInput.classList.add("divInput")
+        divAddImg.appendChild(divInput)
+
+        let imgI = document.createElement("i")
+        imgI.classList.add("far", "fa-image");
+        divInput.appendChild(imgI);
+
+        let imgLabel = document.createElement("label");
+        imgLabel.setAttribute("for", "fileInput")
+        imgLabel.innerText = "+ Ajouter photo"
+        imgLabel.classList.add("imgLabel")
+        divInput.appendChild(imgLabel);
+
+        let imgInput = document.createElement("input");
+        imgInput.setAttribute("type", "file")
+        imgInput.setAttribute("id", "fileInput")
+        imgInput.classList.add("imgInput")
+        imgInput.required = true;
+        imgInput.style.display = "none"
+        divInput.appendChild(imgInput);
+
+        let formatImg = document.createElement("p")
+        formatImg.innerText = "jpg, png : 4mo max"
+        divInput.appendChild(formatImg)
+
+        let previewDiv = document.createElement("div");
+        previewDiv.classList.add("previewDiv")
+        divAddImg.appendChild(previewDiv)
+
+        imgInput.addEventListener("change", function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(event) {
+                    const imageUrl = event.target.result;
+                    const previewImg = document.createElement("img");
+                    previewImg.setAttribute("src", imageUrl);
+                    previewDiv.innerHTML = "";
+                    previewDiv.appendChild(previewImg);
+                }
+                reader.readAsDataURL(file);
+                divInput.style.display = "none"
+            }
+        })
+
+
+
+        let titleProject = document.createElement ("label");
+        titleProject.for = "titleProject";
+        titleProject.innerText = "Titre"
+        form.appendChild(titleProject);
+
+        let titleInput = document.createElement("input");
+        titleInput.type = "text";
+        titleInput.id = "titleInput";
+        titleInput.required = true;
+        form.appendChild(titleInput);
+
+        let categoryForm = document.createElement("label");
+        categoryForm.for = "categorySelect";
+        categoryForm.innerText = "Catégorie";
+        form.appendChild(categoryForm);
+
+        let categorySelect = document.createElement("select");
+        categorySelect.id = "categorySelect";
+        categorySelect.name = "category";
+        categorySelect.required = true;
+
+        async function fetchCategories() {
+            try {
+              const response = await fetch("http://localhost:5678/api/categories");
+              if (!response.status === 200) {
+                throw new Error('Erreur lors de la récupération des catégories');
+              }
+              const categories = await response.json();
+              
+              for (let i = 0; i < categories.length; i++) {
+                let cat = categories[i];
+                let option = document.createElement("option");
+                option.value = cat.id;
+                option.innerText = cat.name;
+                categorySelect.appendChild(option);
+              }
+            } catch (error) {
+              console.error('Erreur lors de la récupération des catégories:', error);
+            }
+        }
+
+        fetchCategories()
+
+        form.appendChild(categorySelect);
+
+        let hrForm = document.createElement("hr");
+        form.appendChild(hrForm)
+
+        let submitWork = document.createElement('input');
+        submitWork.type = 'submit';
+        submitWork.value = 'Valider';
+        submitWork.classList.add('submitWork');
+        submitWork.id = "submitButton"
+        submitWork.disabled = true
+        form.appendChild(submitWork);
+
+        divForm.appendChild(form)
+    }
+
+    checkFormValidity()
+
+    document.querySelector(".divForm").style.display = "inherit";
+
+    console.log("we entered the form")
+
+    const formNewWork = document.querySelector(".submitWork")
+
+    console.log(formNewWork)
+
+    formNewWork.addEventListener("submit", submitNewWork)
+    
+}
+
+
+
+async function submitNewWork(event) {
+
+    console.log("we clicked the button")
+
+    event.preventDefault();
+    
+    const workForm = new FormData();
+
+    workForm.append("image", document.getElementById("imgForm").files[0]);
+    workForm.append("title", document.getElementById("titleProject").value);
+    workForm.append("category", document.getElementById("categoryForm").value);
+
+    console.log(workForm)
+
+    const formResponse = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {Authorization: `Bearer ${tokenID}`},
+        body: workForm
+    })
+
+    console.log(formResponse)
+
+    const formResponseBody = await formResponse.json()
+    if (formResponseBody.status === 201) {
+        console.log("tout va bien")
+    } else {
+        alert("Le Projet n'a pas pu être rajouté avec succès")
+        console.log("raté ^^")
+    }
+    
+}
 
 
 export function openModal(event) {
@@ -73,6 +284,7 @@ export function openModal(event) {
         modal.querySelector(".closeModal").addEventListener("click", closeModal)
         modal.querySelector(".modalStop").removeEventListener("click", stopPropagation)
         document.querySelector(".gridModal").innerHTML = "";
+        document.querySelector(".divForm").style.display = "none";
     }
 
     window.addEventListener("keydown", function (event) {
@@ -81,105 +293,11 @@ export function openModal(event) {
         }
     })
 
-    function previous() { 
-        
-        let previousPage = document.querySelector(".previousPage")
-        if (!previousPage) {
-                
-            previousPage = document.createElement("i")
-            previousPage.classList.add("fa-solid", "fa-arrow-left")
-            previousPage.classList.add("previousPage")
-            let iconCloseModal = document.querySelector(".closeModal")
-            iconCloseModal.parentNode.insertBefore(previousPage, iconCloseModal);
-        }
-        previousPage.addEventListener("click", function () {
-            document.querySelector("#displayModal").style.display = "inherit";
-            previousPage.remove()
-        })
-        
-    }
-
-    function newProject() {
-        previous()
-        document.querySelector("#displayModal").style.display = "none";
-        let divForm = document.createElement("div")
-        divForm.classList.add("divForm")
-        document.querySelector("#divModal").appendChild(divForm)
-        let titleForm = document.createElement("h1")
-        titleForm.innerText = "Ajout photo"
-        titleForm.classList.add("titleForm")
-        divForm.appendChild(titleForm)
-        let form = document.createElement ("form");
-        form.id = "formProject";
-
-        let imgI = document.createElement("i")
-        imgI.classList.add("far", "fa-image");
-        form.appendChild(imgI);
-        let imgForm = document.createElement("label");
-        imgForm.for = "imgForm";
-        form.appendChild(imgForm);
-
-        let imgInput = document.createElement("input");
-        imgInput.type = "file";
-        imgInput.id = "imgForm";
-        imgInput.name = "image";
-        imgInput.accept = "image/*"
-        
-        form.appendChild(imgInput);
-
-        let titleProject = document.createElement ("label");
-        titleProject.for = "titleProject";
-        titleProject.innerText = "Titre"
-        form.appendChild(titleProject);
-
-        let titleInput = document.createElement("input");
-        titleInput.type = "text";
-        titleInput.id = "titleProject";
-        titleInput.name = "title";
-        form.appendChild(titleInput);
-
-        let categoryForm = document.createElement("label");
-        categoryForm.for = "categoryForm";
-        categoryForm.innerText = "Catégorie";
-        form.appendChild(categoryForm);
-
-        let categorySelect = document.createElement("select");
-        categorySelect.id = "categoryForm";
-        categorySelect.name = "category";
-
-        fetch("http://localhost:5678/api/categories")
-        .then(response => response.json())
-        .then(categories => {
-            for (let i = 0; i < categories.length; i++) {
-                let cat = categories[i];
-                let option = document.createElement("option");
-                option.value = cat.id;
-                option.innerText = cat.name;
-                categorySelect.appendChild(option);
-            }
-        })
-        .catch(error => {
-            console.error('Erreur lors de la récupération des catégories:', error);
-        });
-        form.appendChild(categorySelect);
-
-        let hrForm = document.createElement("hr");
-        form.appendChild(hrForm)
-
-        let submitForm = document.createElement('input');
-        submitForm.type = 'submit';
-        submitForm.value = 'Valider';
-        submitForm.id = 'submitForm';
-        form.appendChild(submitForm);
-
-        divForm.appendChild(form)
-    }
-
     let addWork = document.querySelector(".addWork");
-    addWork.addEventListener("click", newProject)
+    addWork.addEventListener("click", newWork)
     
     let deleteWorks = document.querySelectorAll(".deleteWork")
-    let tokenID = localStorage.getItem("tokenID")
+    
 
     for (let i = 0; i < deleteWorks.length; i++) {
         deleteWorks[i].addEventListener("click", async function () {
@@ -206,4 +324,5 @@ export function openModal(event) {
 
         });
     }
+
 }
